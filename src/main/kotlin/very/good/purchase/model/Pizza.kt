63a7ced3db01c9d8fc1ae28bfcd456f6957b4268
green-seagull <9652156+green-seagull@users.noreply.github.com>
2023-08-29 06:30:15 +1000

    Add UUID to Pizza

--- src/main/kotlin/very/good/purchase/model/Pizza.kt
+++ src/main/kotlin/very/good/purchase/model/Pizza.kt
@@ -1,8 +1,10 @@
 package very.good.purchase.model
 
 import java.math.BigDecimal
+import java.util.UUID
 
 data class Pizza(
+    var id: UUID = UUID.randomUUID(),
     var amount: BigDecimal = BigDecimal.ZERO,
     var type:  PizzaType = PizzaType.Plain,
 )
--- src/test/kotlin/very/good/purchase/TestHelpers.kt
+++ src/test/kotlin/very/good/purchase/TestHelpers.kt
@@ -1,12 +1,14 @@
 package very.good.purchase
 
 import java.math.BigDecimal
+import java.util.UUID
 import very.good.purchase.model.Pizza
 import very.good.purchase.model.PizzaType
 
 object TestHelpers {
     fun givenPizza(
+        id: UUID = UUID.randomUUID(),
         amount: BigDecimal = BigDecimal("9.99"),
         type: PizzaType = PizzaType.Hawaiian,
-    ) = Pizza(amount, type)
+    ) = Pizza(id, amount, type)
 }
