8fa9da0c1e5e8b18b6ac888bd4852ee99dc2fa5b
green-seagull <9652156+green-seagull@users.noreply.github.com>
2023-08-29 06:49:10 +1000

    Refactor topology test

--- src/test/kotlin/very/good/purchase/kafka/PizzaToplogyTest.kt
+++ src/test/kotlin/very/good/purchase/kafka/PizzaToplogyTest.kt
@@ -6,25 +6,26 @@ import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG
 import org.apache.kafka.streams.StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG
+import org.apache.kafka.streams.TestInputTopic
 import org.apache.kafka.streams.TopologyTestDriver
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import very.good.purchase.TestHelpers.givenPizza
+import very.good.purchase.model.Pizza
 import very.good.purchase.model.PizzaType
 import very.good.purchase.serdes.PizzaSerdes
 
 class PizzaToplogyTest {
     private lateinit var pizzaToplogy: PizzaToplogy
+    private lateinit var topologyTestDriver: TopologyTestDriver
+    private lateinit var inputTopic: TestInputTopic<String, Pizza>
+    private val streamsBuilder = StreamsBuilder()
 
     @BeforeEach
     fun setup() {
         pizzaToplogy = PizzaToplogy(INBOUND_PIZZA_ORDER_TOPIC)
-    }
 
-    @Test
-    fun `given stream of pizzas, then they are counted`() {
-        val streamsBuilder = StreamsBuilder()
         pizzaToplogy.buildPipeline(streamsBuilder)
         val topology = streamsBuilder.build()
 
@@ -32,13 +33,16 @@ class PizzaToplogyTest {
         config[DEFAULT_KEY_SERDE_CLASS_CONFIG] = Serdes.String()::class.java.name
         config[DEFAULT_VALUE_SERDE_CLASS_CONFIG] = PizzaSerdes::class.java.name
 
-        val topologyTestDriver = TopologyTestDriver(topology, config)
-        val inputTopic = topologyTestDriver.createInputTopic(
+        topologyTestDriver = TopologyTestDriver(topology, config)
+        inputTopic = topologyTestDriver.createInputTopic(
             INBOUND_PIZZA_ORDER_TOPIC,
             StringSerializer(),
             PizzaSerdes.serdes().serializer()
         )
+    }
 
+    @Test
+    fun `given stream of pizzas, then they are counted`() {
         inputTopic.pipeInput("key", givenPizza(type = PizzaType.Hawaiian))
         inputTopic.pipeInput("key", givenPizza(type = PizzaType.Hawaiian))
         inputTopic.pipeInput("key", givenPizza(type = PizzaType.Capricciosa))
